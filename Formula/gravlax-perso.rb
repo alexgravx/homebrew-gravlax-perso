6f22074993b3f0c056be2c656f0a4edc4cad5688
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2025-08-13 00:10:49 +0200

    [FIX] minor fixes and README update

--- Formula/gravlax-perso.rb
+++ Formula/gravlax-perso.rb
@@ -1,18 +1,19 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class HomebrewGravlaxPerso < Formula
+
+class GravlaxPerso < Formula
   desc "A tiny shell script that is installable via homebrew. Get alexgravx github project names and languages"
   homepage "https://github.com/alexgravx/homebrew-gravlax-perso"
   url "https://github.com/alexgravx/homebrew-gravlax-perso/archive/refs/tags/v0.1.tar.gz"
   sha256 "ffdf0ff3b2faa60a26d69008a3c8bf8d61226d6627ce54c04ebf906f7ad517c6"
-  license ""
+  license "MIT"
 
   depends_on "jq"
 
   def install
     # installing the shell script and giving it an executable name
-    bin.install "src/repos.sh" => "gravlax"
+    bin.install "src/repos.sh" => "gravlax-perso"
   end
 
   test do
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# perso-gravlax-homebrew
+# homebrew-gravlax-perso
+
+This is a demo repository for the creation of a homebrew Formula and Tap.
+
+## Quick start
+
+Add the repo with:
+```
+brew tap alexgravx/gravlax-perso
+```
+Install the formula with:
+```
+brew install gravlax-perso
+```
+Last, you can run the command with:
+```
+gravlax-perso
+```
