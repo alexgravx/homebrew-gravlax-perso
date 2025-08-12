438bc4849285fd08d7292f2f7f555e89bc38edf4
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2025-08-13 00:13:13 +0200

    [UPDATE] new release

--- Formula/gravlax-perso.rb
+++ Formula/gravlax-perso.rb
@@ -5,8 +5,8 @@
 class GravlaxPerso < Formula
   desc "A tiny shell script that is installable via homebrew. Get alexgravx github project names and languages"
   homepage "https://github.com/alexgravx/homebrew-gravlax-perso"
-  url "https://github.com/alexgravx/homebrew-gravlax-perso/archive/refs/tags/v0.1.tar.gz"
-  sha256 "ffdf0ff3b2faa60a26d69008a3c8bf8d61226d6627ce54c04ebf906f7ad517c6"
+  url "https://github.com/alexgravx/homebrew-gravlax-perso/archive/refs/tags/v0.2.tar.gz"
+  sha256 "02cc85bd34702e08e21c7d2ecb76384bb17028ee0362052aee5071bb4a5d09f1"
   license "MIT"
 
   depends_on "jq"
